b7d9ce5e25bb5fadaa5490e9da5f4871afa9e3a5
96urkia <132079059+96urkia@users.noreply.github.com>
2026-07-03 08:18:07 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -41,8 +41,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 DB_PATH = os.environ.get("DB_PATH", "gestion_coleccion.db")
 DB_URL = os.environ.get(
     "DB_URL",
-    "https://www.dropbox.com/scl/fi/xpmxz61of4ohqjuzju7e3/bibliotecas_navarra2_filtrado.db?rlkey=dyiscet4s4wts02ffmffj9n6g&st=a1cj2yns&dl=0
-    "?rlkey=fg46liauy6omsq3dkz4gnn5pk&st=jr3xe9k4&dl=1",
+    '''
+    https://www.dropbox.com/scl/fi/xpmxz61of4ohqjuzju7e3/bibliotecas_navarra2_filtrado.db?rlkey=dyiscet4s4wts02ffmffj9n6g&st=jlto2dmf&dl=0
+    
+    ''',
 )
 SESSION_TTL_SECONDS = 3 * 60 * 60  # 3 horas
 ANIO_ACTUAL = 2026
